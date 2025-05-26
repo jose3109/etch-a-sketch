08fed5415c98b613a67366aa7c279bbfef69a00d
Jose Castro <jose.310904@gmail.com>
2025-05-26 16:15:48 -0700

    Complete Etch-a-Sketch project
    Finish the whole javascript logic
    Complete the design of the project with CSS

--- javascript.js
+++ javascript.js
@@ -1,14 +1,42 @@
 
-
-
-function createSquares () {
-    const container = document.querySelector("#container");
-    for (let i = 1; i <= 256; i++) {
-             const square = document.createElement("div");
-            square.classList.add("square");
-            container.appendChild(square); 
+function humanChoice () {
+      let number = parseInt(prompt("How many squares width/height"));
+      return number;
     }
 
+function createSquares () {
+     const container = document.querySelector("#container");
+    const btn = document.querySelector("button"); 
+   
+    btn.addEventListener("click", () => {
+        const squares = container.querySelectorAll(".square")
+        squares.forEach(square => square.remove());
+       let choice = humanChoice();
+          if (choice <= 100 ) {
+            
+        let input = choice;
+             for (let i = 0; i < choice * choice; i++) {
+                const square = document.createElement("div");
+            square.classList.add("square");
+            let size = (500 / input);
+            square.style.cssText = `
+            width: ${size}px;
+            `
+            colorChanger(square);
+            container.appendChild(square); 
+             }  
+            } else {
+                alert("Has to be less than 100");
+             }
+             
+}); 
+}
+
+
+function colorChanger (square) {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "blue";
+    });
 }
 
 createSquares();
